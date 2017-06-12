995d7b7821e3a3d60062caa73dbb9a4f7890fbfd
Manas Gupta <manas.gupta@valiancesolutions.com>
2017-06-12 12:57:47 +0530

    background bar chart

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,73 @@
 $(document).ready(function () {
+    // Background Bar Chart canvas
+    var bgBarCtx = document.getElementById("background-bar-chart");
+    createbgBarChart(bgBarCtx);
 });
+
+// Create Line chart for Total Views
+function createbgBarChart(ctx) {
+    var chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'bar',
+
+        // The data for our dataset
+        data: {
+            labels: ['a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3, 12, 19, 3, 5, 2, 3, 12, 19, 3, 5, 2, 3, 19, 5,12, 19, 3, 5, 2, 3, 12, 19, 3, 5, 2, 3, 12, 19, 3, 5, 2, 3, 19, 5,12, 19, 3, 5, 2, 3, 12, 19, 3, 5, 2, 3, 12, 19, 3, 5, 2, 3, 19, 5,12, 19, 3, 5, 2, 3, 12, 19, 3, 5, 2, 3, 12, 19, 3, 5, 2, 3, 19, 5,12, 19, 3, 5, 2, 3, 12, 19, 3, 5, 2, 3, 12, 19, 3, 5, 2, 3, 19, 5],
+                backgroundColor: 'rgba(219, 221, 225, 1)',
+                borderColor: 'rgba(219, 221, 225, 1)',
+                borderWidth: 1
+            }]
+        },
+
+        // Configuration options go here
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            responsiveAnimationDuration: 400,  // in ms
+            title: {
+                text: 'Data',
+                display: false,
+                fontColor: '#fff',
+                fontStyle: 'normal'
+            },
+            legend: {
+                display: false
+            },
+            tooltips: {
+                enabled: false
+            },
+            scales: {
+                yAxes: [{
+                    // display: false,
+                    gridLines: {
+                        color: 'rgba(255, 255, 255, 0.3)',
+                        lineWidth: 2,
+                        drawBorder: false
+                    },
+                    ticks: {
+                        beginAtZero: true,
+                        fontColor: 'rgba(255, 255, 255, 0.7)',
+                        padding: 10
+                    },
+                    display: false
+                }],
+                xAxes: [{
+                    categoryPercentage: 1.0,
+                    barPercentage: 0.75,
+                    gridLines: {
+                        color: 'rgba(255, 255, 255, 0.3)',
+                        lineWidth: 2,
+                        drawBorder: false
+                    },
+                    ticks: {
+                        display: false
+                    },
+                    display: false
+                }]
+            }
+        }
+    });
+}
