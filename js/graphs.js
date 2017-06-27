a0ded5985d85b1f14f7ce56aa50800eaa3900643
Manas Gupta <manas.gupta@valiancesolutions.com>
2017-06-27 12:20:24 +0530

    remove banner background and its bar chart, make main container full width of screen untill 1400px and beyond that fixed width of 1400px

--- js/graphs.js
+++ js/graphs.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     // Background Bar Chart canvas
-    var bgBarCtx = document.getElementById("background-bar-chart");
-    createbgBarChart(bgBarCtx);
+    // var bgBarCtx = document.getElementById("background-bar-chart");
+    // createbgBarChart(bgBarCtx);
 
     // % usage every month in different categories line chart
     var perCatUse = document.getElementById("perCatLineChart");
@@ -32,88 +32,88 @@ $(document).ready(function () {
 });
 
 // Create Line chart for Total Views
-function createbgBarChart(ctx) {
-    var chart = new Chart(ctx, {
-        // The type of chart we want to create
-        type: 'bar',
-
-        // The data for our dataset
-        data: {
-            labels: ['a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a'],
-            datasets: [{
-                label: '# of Votes',
-                data: [26, 34, 22, 43, 6, 25, 40, 45, 9, 33, 35, 29, 17, 9, 45, 2, 28, 16, 12, 39, 3, 31, 11, 42, 49, 20, 22, 5, 45, 26, 7, 14, 28, 12, 19, 44, 40, 46, 24, 37, 36, 3, 8, 25, 18, 11, 32, 48, 27, 38, 38, 7, 39, 47, 48, 34, 13, 26, 50, 18, 28, 24, 23, 29, 49, 42, 1, 30, 12, 35, 41, 40, 36, 19, 22, 43, 6, 25, 40, 45, 9, 27, 44, 17, 18, 36, 4, 5, 16, 34, 48, 13, 7, 50, 42, 24, 26, 32, 30, 21],
-                backgroundColor: 'rgba(219, 221, 225, 1)',
-                borderColor: 'rgba(219, 221, 225, 1)',
-                hoverBackgroundColor: 'rgba(210, 210, 210, 1)',
-                hoverBorderColor: 'rgba(210, 210, 210, 1)',
-                borderWidth: 1
-            }]
-        },
-
-        // Configuration options go here
-        options: {
-            responsive: true,
-            maintainAspectRatio: false,
-            // responsiveAnimationDuration: 400,  // in ms
-            animation: {
-                duration: 700,
-                easing: 'easeInQuint'
-            },
-            title: {
-                text: 'Data',
-                display: false,
-                fontColor: '#fff',
-                fontStyle: 'normal'
-            },
-            legend: {
-                display: false
-            },
-            tooltips: {
-                enabled: false
-            },
-            scales: {
-                yAxes: [{
-                    // display: false,
-                    gridLines: {
-                        color: 'rgba(255, 255, 255, 0.3)',
-                        lineWidth: 2,
-                        drawBorder: false
-                    },
-                    ticks: {
-                        beginAtZero: true,
-                        fontColor: 'rgba(255, 255, 255, 0.7)',
-                        padding: 10
-                    },
-                    display: false
-                }],
-                xAxes: [{
-                    categoryPercentage: 1.0,
-                    barPercentage: 0.75,
-                    gridLines: {
-                        color: 'rgba(255, 255, 255, 0.3)',
-                        lineWidth: 2,
-                        drawBorder: false
-                    },
-                    ticks: {
-                        display: false
-                    },
-                    display: false
-                }]
-            }
-        }
-    });
-
-    setInterval(function() {
-        // console.log(chart.data.datasets[0].data);
-        for (var i = 0; i < 7; i++) {
-            var index = Math.floor(Math.random() * 100);
-            var dataVal = Math.ceil(Math.random() * 50);
-            chart.data.datasets[0].data[index] = dataVal;
-        }
-        chart.update();
-    }, 3000);
-}
+// function createbgBarChart(ctx) {
+//     var chart = new Chart(ctx, {
+//         // The type of chart we want to create
+//         type: 'bar',
+//
+//         // The data for our dataset
+//         data: {
+//             labels: ['a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a'],
+//             datasets: [{
+//                 label: '# of Votes',
+//                 data: [26, 34, 22, 43, 6, 25, 40, 45, 9, 33, 35, 29, 17, 9, 45, 2, 28, 16, 12, 39, 3, 31, 11, 42, 49, 20, 22, 5, 45, 26, 7, 14, 28, 12, 19, 44, 40, 46, 24, 37, 36, 3, 8, 25, 18, 11, 32, 48, 27, 38, 38, 7, 39, 47, 48, 34, 13, 26, 50, 18, 28, 24, 23, 29, 49, 42, 1, 30, 12, 35, 41, 40, 36, 19, 22, 43, 6, 25, 40, 45, 9, 27, 44, 17, 18, 36, 4, 5, 16, 34, 48, 13, 7, 50, 42, 24, 26, 32, 30, 21],
+//                 backgroundColor: 'rgba(219, 221, 225, 1)',
+//                 borderColor: 'rgba(219, 221, 225, 1)',
+//                 hoverBackgroundColor: 'rgba(210, 210, 210, 1)',
+//                 hoverBorderColor: 'rgba(210, 210, 210, 1)',
+//                 borderWidth: 1
+//             }]
+//         },
+//
+//         // Configuration options go here
+//         options: {
+//             responsive: true,
+//             maintainAspectRatio: false,
+//             // responsiveAnimationDuration: 400,  // in ms
+//             animation: {
+//                 duration: 700,
+//                 easing: 'easeInQuint'
+//             },
+//             title: {
+//                 text: 'Data',
+//                 display: false,
+//                 fontColor: '#fff',
+//                 fontStyle: 'normal'
+//             },
+//             legend: {
+//                 display: false
+//             },
+//             tooltips: {
+//                 enabled: false
+//             },
+//             scales: {
+//                 yAxes: [{
+//                     // display: false,
+//                     gridLines: {
+//                         color: 'rgba(255, 255, 255, 0.3)',
+//                         lineWidth: 2,
+//                         drawBorder: false
+//                     },
+//                     ticks: {
+//                         beginAtZero: true,
+//                         fontColor: 'rgba(255, 255, 255, 0.7)',
+//                         padding: 10
+//                     },
+//                     display: false
+//                 }],
+//                 xAxes: [{
+//                     categoryPercentage: 1.0,
+//                     barPercentage: 0.75,
+//                     gridLines: {
+//                         color: 'rgba(255, 255, 255, 0.3)',
+//                         lineWidth: 2,
+//                         drawBorder: false
+//                     },
+//                     ticks: {
+//                         display: false
+//                     },
+//                     display: false
+//                 }]
+//             }
+//         }
+//     });
+//
+//     setInterval(function() {
+//         // console.log(chart.data.datasets[0].data);
+//         for (var i = 0; i < 7; i++) {
+//             var index = Math.floor(Math.random() * 100);
+//             var dataVal = Math.ceil(Math.random() * 50);
+//             chart.data.datasets[0].data[index] = dataVal;
+//         }
+//         chart.update();
+//     }, 3000);
+// }
 
 // Create Line chart for % usage per month category wise
 function createPerCatUseChart(ctx) {
